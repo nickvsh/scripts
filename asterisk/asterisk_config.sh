968d6cccddd3d9345165ba97e18027441a38d34f
Nick <mail@in.ua>
2017-07-03 17:45:51 +0300

    check file

--- asterisk/asterisk_config.sh
+++ asterisk/asterisk_config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-yum install ntp && ntpdate pool.ntp.org && systemctl enable ntpd && systemctl start ntpd
-
-useradd asterisk && passwd asterisk
-chown -R asterisk:asterisk {/var/run, /var/spool, /var/lib,/var/log}/asterisk
+#yum install ntp && ntpdate pool.ntp.org && systemctl enable ntpd && systemctl start ntpd
+#adduser --system --home /usr/sbin --no-create-home --disabled-password  --group asterisk --gecos "Asterisk PBX user"
+chown -R asterisk:asterisk {/var/run,/var/spool,/var/lib,/var/log}/asterisk
 
