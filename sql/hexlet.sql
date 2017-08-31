e6f38dbb3c6d74174d0d1b6f8ea10a047c5cb725
Nick V <nick@vashchenko.in.ua>
2017-09-01 01:41:02 +0300

    pk = unique + not null

--- sql/hexlet.sql
+++ sql/hexlet.sql
@@ -1,7 +1,8 @@
 DROP TABLE products;
 
 CREATE TABLE products (
-	product_no integer,
+	--product_no integer NOT NULL,
+	product_no integer PRIMARY key, -- = UNIQUE + NOT NULL tech in psql,
 	product boolean,
 	name varchar(20), -- it's alias to character varying
 	--name character varying(12),
@@ -9,4 +10,5 @@ CREATE TABLE products (
 	count integer,
 	price numeric,
 	created_at timestamp
+	,UNIQUE(product_no, count)
 );
