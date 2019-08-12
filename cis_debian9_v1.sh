5ecd6015375131154d2013031e357e7dd0709505
Nick <nickvsh@nicks-mbp.pzu.local>
2019-08-12 15:04:11 +0300

    done till 3.4

--- cis_debian9_v1.sh
+++ cis_debian9_v1.sh
@@ -47,20 +47,50 @@ cis_text=(
 "\t	1.2 Configure Software Updates\n
 \t\t	1.2.1 Ensure package manager repositories are configured (Not Scored)"
 "\t\t 1.2.2 Ensure GPG keys are configured (Not Scored)"
-"\t 1.3 Filesystem Integrity Checking"
 "\t	1.4 Secure Boot Settings\n
 \t\t	1.4.1 Ensure permissions on bootloader config are configured (Scored)"
 "\t\t 1.4.2 Ensure bootloader password is set (Scored)"
 "\t\t 1.4.3 Ensure authentication required for single user mode (Scored)"
 "\t	1.5 Additional Process Hardening\n
 \t\t	1.5.1 Ensure core dumps are restricted (Scored)"
+"\t\t	1.5.3 Ensure address space layout randomization (ASLR) is enabled (Scored)"
+"\t 1.7 Warning Banners\n
+\t\t	1.7.1 Command Line Warning Banners\n
+\t\t\t	1.7.1.1 Ensure message of the day is configured properly (Scored)"
+"\t\t\t 1.7.1.2 Ensure local login warning banner is configured properly (Scored)"
+"\t\t\t 1.7.1.3 Ensure remote login warning banner is configured properly (Scored)"
+"\t\t\t 1.7.1.4 Ensure permissions on /etc/motd are configured (Scored)"
+"\t\t\t 1.7.1.5 Ensure permissions on /etc/issue are configured (Scored)"
+"\t\t\t 1.7.1.6 Ensure permissions on /etc/issue.net are configured (Scored)"
+"3 Network Configuration\n
+\t 3.1 Network Parameters (Host Only)\n
+\t\t\t 3.1.1 Ensure IP forwarding is disabled (Scored)"
+"\t\t\t 3.1.2 Ensure packet redirect sending is disabled (Scored)"
+"\t 3.2 Network Parameters (Host and Router)\n
+\t\t	3.2.1 Ensure source routed packets are not accepted (Scored)"
+"\t\t	3.2.2 Ensure ICMP redirects are not accepted (Scored)"
+"\t\t	3.2.3 Ensure secure ICMP redirects are not accepted (Scored)"
+"\t\t	3.2.4 Ensure suspicious packets are logged (Scored)"
+"\t\t	3.2.5 Ensure broadcast ICMP requests are ignored (Scored)"
+"\t\t	3.2.6 Ensure bogus ICMP responses are ignored (Scored)"
+"\t\t	3.2.7 Ensure Reverse Path Filtering is enabled (Scored)"
+"\t\t	3.2.8 Ensure TCP SYN Cookies is enabled (Scored)"
+"\t\t	3.2.9 Ensure IPv6 router advertisements are not accepted (Scored)"
+"\t 3.3 TCP Wrappers\n
+\t\t 3.3.1 Ensure TCP Wrappers is installed (Scored)"
+"\t\t 3.3.2 Ensure /etc/hosts.allow is configured (Not Scored)"
+"\t\t 3.3.3 Ensure /etc/hosts.deny is configured (Not Scored)"
+"\t\t 3.3.4 Ensure permissions on /etc/hosts.allow are configured (Scored)"
+"\t\t 3.3.5 Ensure permissions on /etc/hosts.deny are configured (Scored)"
+# "\t 3.4 Uncommon Network Protocols
+# \t\t 3.4.1 Ensure DCCP is disabled (Not Scored)"
 
 # "\t "
 # "\t\t "
+#UP
 # "\t\t\t "
 
 )
-#UP
 
 # 'echo -e "#CIS-${cis_number[1]}\n""install #1 /bin/true" > /etc/modprobe.d/command1.conf'
 # 'echo -e "#CIS-1.1.1.1\n" "install freevxfs /bin/true" > /etc/modprobe.d/freevxfs.conf'
@@ -208,12 +238,159 @@ grep "^set superusers=\"pzu-grub\"" /boot/grub/grub.cfg && grep "^password_pbkdf
 grep ^root:[*\!]: /etc/shadow
 '
 'scored=true
-echo "* hard core 0" >> /etc/security/limits.conf
-echo "fs.suid_dumpable = 0" >> /etc/sysctl.conf
+echo "* hard core 0" >> /etc/security/limits.d/cis_coredump.conf
+echo "fs.suid_dumpable = 0" > /etc/sysctl.d/cis_coredump.conf
 sysctl -w fs.suid_dumpable=0
 grep "hard core" /etc/security/limits.conf /etc/security/limits.d/* && grep "fs\.suid_dumpable" /etc/sysctl.conf /etc/sysctl.d/*
 '
-
+'scored=true
+sysctl -w kernel.randomize_va_space=2
+echo "kernel.randomize_va_space = 2" > /etc/sysctl.d/cis_kernel_ASLR.conf
+'
+'scored=true
+cat <<EOF > /etc/motd
+The device is private property of PZU.
+Only PZU System Administrators are allowed to setup the device.
+Illegal access to computer network will be prosecuted by law.
+EOF
+'
+'scored=true
+echo "Authorized uses only. All activity may be monitored and reported." > /etc/issue
+'
+'scored=true
+echo "Authorized uses only. All activity may be monitored and reported." > /etc/issue.net
+'
+'scored=true
+chown root:root /etc/motd && chmod 644 /etc/motd
+'
+'scored=true
+chown root:root /etc/issue && chmod 644 /etc/issue
+'
+'scored=true
+chown root:root /etc/issue.net && chmod 644 /etc/issue.net
+'
+'scored=true
+sysctl -w net.ipv4.ip_forward=0
+sysctl -w net.ipv6.conf.all.forwarding=0
+sysctl -w net.ipv4.route.flush=1
+sysctl -w net.ipv6.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_ip_forwarding_is_disabled.conf
+net.ipv4.ip_forward = 0
+net.ipv6.conf.all.forwarding = 0
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.conf.all.send_redirects=0
+sysctl -w net.ipv4.conf.default.send_redirects=0
+sysctl -w net.ipv4.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_packet_redirect_sending_is_disabled.conf
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.conf.all.accept_source_route=0
+sysctl -w net.ipv4.conf.default.accept_source_route=0
+sysctl -w net.ipv6.conf.all.accept_source_route=0
+sysctl -w net.ipv6.conf.default.accept_source_route=0
+sysctl -w net.ipv4.route.flush=1
+sysctl -w net.ipv6.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_source_routed_packets.conf
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv6.conf.all.accept_source_route = 0
+net.ipv6.conf.default.accept_source_route = 0
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.default.accept_redirects=0
+sysctl -w net.ipv6.conf.all.accept_redirects=0
+sysctl -w net.ipv6.conf.default.accept_redirects=0
+sysctl -w net.ipv4.route.flush=1
+sysctl -w net.ipv6.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_icmp_redirects.conf
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.conf.all.secure_redirects=0
+sysctl -w net.ipv4.conf.default.secure_redirects=0
+sysctl -w net.ipv4.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_secure_icmp_redirects.conf
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.conf.all.log_martians=1
+sysctl -w net.ipv4.conf.default.log_martians=1
+sysctl -w net.ipv4.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_suspicious_packets.conf
+net.ipv4.conf.all.log_martians = 1
+net.ipv4.conf.default.log_martians = 1
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+sysctl -w net.ipv4.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_broadcast_packets.conf
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
+sysctl -w net.ipv4.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_bogus_icmp.conf
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.conf.all.rp_filter=1
+sysctl -w net.ipv4.conf.default.rp_filter=1
+sysctl -w net.ipv4.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_reverse_path_filtering.conf
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+EOF
+'
+'scored=true
+sysctl -w net.ipv4.tcp_syncookies=1
+sysctl -w net.ipv4.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_tcp_syn_cookies.conf
+net.ipv4.tcp_syncookies = 1
+EOF
+'
+'scored=true
+sysctl -w net.ipv6.conf.all.accept_ra=0
+sysctl -w net.ipv6.conf.default.accept_ra=0
+sysctl -w net.ipv6.route.flush=1
+cat <<EOF > /etc/sysctl.d/cis_ipv6_router_advertisements
+net.ipv6.conf.all.accept_ra = 0
+net.ipv6.conf.default.accept_ra = 0
+EOF
+'
+'scored=true
+apt-get install tcpd
+dpkg -s tcpd
+'
+'scored=false
+echo "ALL: 192.168.13.0/255.255.255.0, 192.168.25.0/255.255.255.0" > /etc/hosts.allow
+'
+'scored=true
+echo "ALL: ALL" > /etc/hosts.deny
+'
+'scored=true
+chown root:root /etc/hosts.allow && chmod 644 /etc/hosts.allow
+'
+'scored=true
+chown root:root /etc/hosts.deny && chmod 644 /etc/hosts.deny
+'
+# 'scored=true
+# '
 )
 #UP
 # mount | grep /tmp | grep noexec
